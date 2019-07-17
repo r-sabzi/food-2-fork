d21e2a4bef9edd87e63ec26c7e375e7577ed5ea9
r.sabzi <r.kh.sabzi@gmail.com>
2019-07-17 20:18:33 +0430

    add navbar

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import Recipes from "./pages/Recipes";
 import SingleRecipe from "./pages/SingleRecipe";
 import Default from "./pages/Default";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import Navbar from "./components/Navbar";
 class App extends Component {
   render() {
     return (
       <Router>
         <main>
+          <Navbar />
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/recipes" exact component={Recipes} />
